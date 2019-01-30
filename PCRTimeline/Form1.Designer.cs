f905b97223efe191e560e3b025c020029fe8fae8
izmktr <izmktr@gmail.com>
2019-01-30 15:54:04 +0900

    FirstCommit

---
+++ PCRTimeline/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PCRTimeline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.iconScrollBar = new System.Windows.Forms.VScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.iconScrollBar);
+            this.splitContainer1.Panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.splitContainer1_Panel2_DragEnter);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.splitContainer1_Panel2_MouseDown);
+            this.splitContainer1.Panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.splitContainer1_Panel2_MouseMove);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(797, 263);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // iconScrollBar
+            // 
+            this.iconScrollBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.iconScrollBar.Location = new System.Drawing.Point(775, 0);
+            this.iconScrollBar.Name = "iconScrollBar";
+            this.iconScrollBar.Size = new System.Drawing.Size(25, 180);
+            this.iconScrollBar.TabIndex = 0;
+            this.iconScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.iconScrollBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.VScrollBar iconScrollBar;
+    }
+}
+
---
+++ PCRTimeline/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCRTimeline
+{
+    public partial class Form1 : Form
+    {
+        List<Avatar> avatarlist = new List<Avatar>();
+        System.Windows.Forms.ImageList imageList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imageList = new System.Windows.Forms.ImageList();
+        }
+
+        void AvatarLoad()
+        {
+            const string path = @"Data\Icon\";
+            string[] files = System.IO.Directory.GetFiles(path, "*.png", System.IO.SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                avatarlist.Add(new Avatar()
+                {
+                    Name = file,
+                    image = Image.FromFile(file)
+                });
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                avatarlist.Add(avatarlist[0]);
+                avatarlist.Add(avatarlist[1]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AvatarLoad();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Control c = (Control)sender;
+            int width = c.Width - iconScrollBar.Width;
+            int height = c.Height;
+
+            int x = 0, y = 0, maxheight = 0;
+            foreach (var avatar in avatarlist)
+            {
+                Image image = avatar.image;
+
+                //DrawImageメソッドで画像を座標(0, 0)の位置に表示する
+                e.Graphics.DrawImage(image, x, y - iconScrollBar.Value, image.Width, image.Height);
+                x += image.Width;
+
+                maxheight = Math.Max(maxheight, image.Height);
+                if (0 < x && width - image.Width <= x)
+                {
+                    x = 0;
+                    y += maxheight;
+                    maxheight = 0;
+                }
+            }
+
+            iconScrollBar.Maximum = y + maxheight;
+            iconScrollBar.LargeChange = height;
+
+        }
+
+        private void iconScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            splitContainer1.Panel2.Invalidate();
+        }
+
+
+        Point mouseDownPoint = Point.Empty;
+
+        private void splitContainer1_Panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDownPoint != Point.Empty)
+            {
+                Rectangle dragRegion = new Rectangle(
+                    mouseDownPoint.X - SystemInformation.DragSize.Width / 2,
+                    mouseDownPoint.Y - SystemInformation.DragSize.Height / 2,
+                    SystemInformation.DragSize.Width,
+                    SystemInformation.DragSize.Height);
+                if (!dragRegion.Contains(e.X, e.Y))
+                {
+                    var image = avatarlist[0].image;
+
+
+                    // Imageの初期化 
+                    imageList.Images.Clear();
+                    imageList.ImageSize = new Size(image.Width, image.Height);
+
+                    // 半透明イメージの元画像を作成、ImageListに追加 
+                    imageList.Images.Add(image);
+
+                    // ImageList_BeginDragにはドラッグする
+                    // イメージの中における相対座標を指定する 
+                    if (Win32ImageList.ImageList_BeginDrag(imageList.Handle, 0,
+                                                     e.X,
+                                                     e.Y))
+                    {
+                        Win32ImageList.ImageList_EndDrag();
+                    }
+                    mouseDownPoint = Point.Empty;
+                }
+
+            }
+        }
+
+        private void splitContainer1_Panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void splitContainer1_Panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            // ImageList_DragEnterにはクライアント領域における相対座標ではなく 
+            // タイトルバーなどの非クライアント領域を含むWindowにおける相対座標を指定する 
+            Point p = this.PointToClient(Cursor.Position);
+            int x = Cursor.Position.X - this.Left;
+            int y = Cursor.Position.Y - this.Top;
+            // ドラッグ中は半透明イメージを表示し続けたいのでImageList_DragEnterには 
+            // ListBoxのHandleを渡すのでなく、FormのHandleを渡す 
+            Win32ImageList.ImageList_DragEnter(this.Handle, x, y);
+        }
+    }
+}
+
